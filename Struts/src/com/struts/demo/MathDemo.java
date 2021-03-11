5704440524be17153c5baeccfcdd4120b468230d
shihaojieHaHa <2690631906@qq.com>
2021-03-11 11:09:41 +0800

    初始Math类

--- Struts/src/com/struts/demo/MathDemo.java
+++ Struts/src/com/struts/demo/MathDemo.java
@@ -2,7 +2,6 @@ package com.struts.demo;
 
 public class MathDemo {
     public static void main(String[] args) {
-        System.out.println("------------");
         System.out.println(1 + 2 / 3 - 4 * 5);
     }
 }
