c998db2fe778b13d2736cb34173fe4d6741d026c
shihaojieHaHa <2690631906@qq.com>
2021-03-15 17:19:27 +0800

    对Action中特定方法进行校验消息action

---
+++ Struts/src/com/struts/personal/PersonalOtherAction.java
@@ -0,0 +1,47 @@
+package com.struts.personal;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.regex.Pattern;
+
+public class PersonalOtherAction extends ActionSupport {
+    private String username;
+    private String mobile;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String update(){
+        ActionContext.getContext().put("message", "更新成功");
+        return "message";
+    }
+
+    public String save(){
+        ActionContext.getContext().put("message", "保存成功");
+        return "message";
+    }
+    //提供会对update()方法校验
+    public void validateUpdate() {
+        if(this.username==null || "".equals(this.username.trim())){
+            this.addFieldError("username", "用户名不能为空");
+        }
+        if(this.mobile==null || "".equals(this.mobile.trim())){
+            this.addFieldError("mobile", "手机号不能为空");
+        }else{
+            if(!Pattern.compile("^1[358]\\d{9}$").matcher(this.mobile).matches()){
+                this.addFieldError("mobile", "手机号格式不正确");
+            }
+        }
+    }
+
+}
