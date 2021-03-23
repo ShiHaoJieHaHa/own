21187c26c44519680be8c033762647ca7ab875e0
shihaojieHaHa <2690631906@qq.com>
2021-03-23 14:04:00 +0800

    修改Action范围国际化action

--- native/src/com/internationalization/placeholderAction.java
+++ native/src/com/internationalization/placeholderAction.java
@@ -3,11 +3,12 @@ package com.internationalization;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
+
 public class placeholderAction extends ActionSupport {
     @Override
     public String execute() throws Exception {
-        ActionContext.getContext().put("message",this.getText("Come",
-                new String[]{"lining","study"}));
+        ActionContext.getContext().put("message", this.getText("Come",
+                new String[]{"lining", "study"}));
         return "message";
     }
 }
