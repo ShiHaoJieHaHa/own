9fb6d6f4762059a4e1f3ad45fbb31b083debd903
shihaojieHaHa <2690631906@qq.com>
2021-05-07 17:04:43 +0800

    实体类的Action

---
+++ SSH/src/cn/demo/action/EmployeeManageAction.java
@@ -0,0 +1,34 @@
+package cn.demo.action;
+
+import javax.annotation.Resource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionContext;
+
+import cn.demo.bean.Employee;
+import cn.demo.service.EmployeeService;
+
+@Controller @Scope("prototype")
+public class EmployeeManageAction {
+	@Resource EmployeeService employeeService;
+	private Employee employee;	
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public String addUI(){
+		return "add";
+	}
+	
+	public String add(){
+		employeeService.save(employee);
+		ActionContext.getContext().put("message", "±£´æ³É¹¦");
+		return "message";
+	}
+}
