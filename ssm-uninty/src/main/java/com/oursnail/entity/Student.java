1353a08a12f47345cd9f83c3e4850befcbd3afb6
shihaojieHaHa <2690631906@qq.com>
2021-05-27 14:28:00 +0800

    ssm项目pojo

---
+++ ssm-uninty/src/main/java/com/oursnail/entity/Student.java
@@ -0,0 +1,45 @@
+package com.oursnail.entity;
+
+import java.util.Date;
+
+public class Student {
+    private Long sid;
+
+    private String sname;
+
+    private String ssex;
+
+    private Date sbirthday;
+
+    public Long getSid() {
+        return sid;
+    }
+
+    public void setSid(Long sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname == null ? null : sname.trim();
+    }
+
+    public String getSsex() {
+        return ssex;
+    }
+
+    public void setSsex(String ssex) {
+        this.ssex = ssex == null ? null : ssex.trim();
+    }
+
+    public Date getSbirthday() {
+        return sbirthday;
+    }
+
+    public void setSbirthday(Date sbirthday) {
+        this.sbirthday = sbirthday;
+    }
+}
