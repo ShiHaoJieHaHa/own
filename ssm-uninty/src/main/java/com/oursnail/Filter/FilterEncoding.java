59b7b563f609a70f43b514e9ec87c1a109755e74
shihaojieHaHa <2690631906@qq.com>
2021-05-27 14:28:23 +0800

    ssm项目filter

---
+++ ssm-uninty/src/main/java/com/oursnail/Filter/FilterEncoding.java
@@ -0,0 +1,33 @@
+package com.oursnail.Filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FilterEncoding implements Filter {
+     public static  String encoding = null;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        if (encoding != null) {
+            HttpServletRequest rt=(HttpServletRequest)request;
+            HttpServletResponse re=(HttpServletResponse)response;
+            rt.setCharacterEncoding(encoding);
+            re.setContentType("text/html;charset=" + encoding);
+            filterChain.doFilter(request, response);
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+        encoding=null;
+    }
+}
