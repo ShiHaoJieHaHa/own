1217fc6b7751e97dd558801f4bb391752b4d74fe
shihaojieHaHa <2690631906@qq.com>
2021-07-06 14:46:02 +0800

    test测试

---
+++ other/src/three/Test.java
@@ -0,0 +1,13 @@
+package three;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = x++;
+        int a=5;
+        int b=++a;
+        System.out.println("x="+x+ "y=" + y);
+        System.out.println("a="+a+ "b=" + b);
+    }
+
+}
