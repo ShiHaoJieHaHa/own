d7a7a62d80ab5f3804463bc9bf7bc2dfde9b2fc4
shihaojieHaHa <2690631906@qq.com>
2021-07-07 15:13:20 +0800

    冒泡排序的第二种写法

--- other/src/sort/BubbleSort.java
+++ other/src/sort/BubbleSort.java
@@ -5,19 +5,35 @@ public class BubbleSort {
 
     }
 
-    public static   void  sort(int[] a ){
-        int  temp=0;
-        for (int i=1;i<a.length-1;i++){
-            for (int j=1;j<a.length-1;j++){
-                if (a[j+1]>a[j]){
-                    temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+    public static void sort(int[] a) {
+        int temp = 0;
+        for (int i = 1; i < a.length - 1; i++) {
+            for (int j = 1; j < a.length - 1; j++) {
+                if (a[j + 1] > a[j]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
+    }
 
-
+    public static void sort2(int[] a) {
+        int flag = 0;
+        int temp = 0;
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j < a.length; j++) {
+                if (a[j + 1] < a[j]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                break;
+            }
+        }
     }
 
 
