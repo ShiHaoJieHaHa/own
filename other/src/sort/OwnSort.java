dc7b47718ddfcb55f04abf54aa421b3329027c36
shihaojieHaHa <2690631906@qq.com>
2021-07-08 16:27:14 +0800

    own冒泡排序..

--- other/src/sort/OwnSort.java
+++ other/src/sort/OwnSort.java
@@ -19,7 +19,7 @@ public class OwnSort {
         int temp = 0;
         int flag = 0;
         for (int i = 1; i < arr.length - 1; i++) {
-            for (int j = 1; j < arr.length - 1; j++) {
+            for (int j = 1; j < arr.length - 1-i; j++) {
                 if (arr[j + 1] < arr[j]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
